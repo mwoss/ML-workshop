e1740c0608120c3e2471dda8272c4a133fea72aa
mwoss <wos.mateusz16@gmail.com>
2018-03-07 01:08:11 +0100

    Learning curves, regiralizaton linear regression, bias vs variance

---
+++ Coursera-machine-learning-ex5/ex5/polyFeatures.m
@@ -0,0 +1,28 @@
+function [X_poly] = polyFeatures(X, p)
+%POLYFEATURES Maps X (1D vector) into the p-th power
+%   [X_poly] = POLYFEATURES(X, p) takes a data matrix X (size m x 1) and
+%   maps each example into its polynomial features where
+%   X_poly(i, :) = [X(i) X(i).^2 X(i).^3 ...  X(i).^p];
+%
+
+
+% You need to return the following variables correctly.
+X_poly = zeros(numel(X), p);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Given a vector X, return a matrix X_poly where the p-th 
+%               column of X contains the values of X to the p-th power.
+%
+% 
+
+%im retarded xD it took me 20min to figured it out that i power every value to p not iter power xD
+for iter=1:p
+  X_poly(:,iter) = X .^ iter;
+end
+
+
+
+
+% =========================================================================
+
+end
